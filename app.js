83383539fe0043e43cfe20488ce716b91491d765
AMJaneiro <114472204+AMJaneiro@users.noreply.github.com>
2023-02-01 22:43:43 +0000

    Applied regex in date validation

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ function validatePhone() {
 bornDate.addEventListener("input", validateBornDate);
 
 function validateBornDate() {
-    let regexBornDate = (/(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d{2}/);
+    let regexBornDate = (/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/);
     let bornDateValue = bornDate.value;
     let crossBornDate = document.getElementsByClassName("cross")[3];
 
